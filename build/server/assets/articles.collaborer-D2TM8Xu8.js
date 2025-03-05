bea7d0de1f4c755c6d870e87bb3bfa1aed34dfd9
herdofpane <121870569+herdofpane@users.noreply.github.com>
2025-03-05 21:20:50 +0100

    maj

---
+++ build/server/assets/articles.collaborer-D2TM8Xu8.js
@@ -0,0 +1,4 @@
+const articles_collaborer = "---\r\ntitle: 'Collaborer'\r\nabstract: 'Travail en équipe et management informatique.'\r\ndate: '2024-03-05'\r\nbanner: '/static/collaborer-banner.jpg'\r\nfeatured: false\r\nniveaux:\r\n  - 'Niveau 1 : Identifier ses aptitudes pour travailler en équipe.'\r\n  - 'Niveau 2 : Situer son rôle et ses missions dans une équipe.'\r\n  - 'Niveau 3 : Manager une équipe informatique.'\r\n---\r\n";
+export {
+  articles_collaborer as default
+};
---
+++ build/server/assets/articles.optimiser-BTLwsiPC.js
@@ -0,0 +1,4 @@
+const articles_optimiser = "---\r\ntitle: 'Optimiser'\r\nabstract: 'Sélection et amélioration des algorithmes et applications.'\r\ndate: '2024-03-05'\r\nbanner: '/static/optimiser-banner.jpg'\r\nfeatured: false\r\nniveaux:\r\n  - 'Niveau 1 : Appréhender et analyser les algorithmes.'\r\n  - 'Niveau 2 : Sélectionner les algorithmes adéquats.'\r\n  - 'Niveau 3 : Analyser et optimiser des applications.'\r\n---\r\n";
+export {
+  articles_optimiser as default
+};
