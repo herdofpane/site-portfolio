1bb7e5b91ad5bfd46cf6b714888d398dc96ad0f8
herdofpane <121870569+herdofpane@users.noreply.github.com>
2025-03-05 21:44:44 +0100

    Updates

---
+++ assets/articles.modern-styling-in-react-Brg0qKzz.js
@@ -0,0 +1,20 @@
+import{j as n}from"./jsx-runtime-DexIYAB0.js";import{u as l}from"./index-L_4ix5aQ.js";function i(s){const e={em:"em",h2:"h2",li:"li",strong:"strong",ul:"ul",...l(),...s.components};return n.jsxs(n.Fragment,{children:[n.jsx(e.h2,{id:"️-développement",children:"🖥️ Développement"}),`
+`,n.jsxs(e.ul,{children:[`
+`,n.jsxs(e.li,{children:[n.jsx(e.strong,{children:"Langages"})," : Java, C, C++, Python ",n.jsx(e.em,{children:"(notions en Django)"}),", JavaFX"]}),`
+`]}),`
+`,n.jsx(e.h2,{id:"-web",children:"🌐 Web"}),`
+`,n.jsxs(e.ul,{children:[`
+`,n.jsxs(e.li,{children:[n.jsx(e.strong,{children:"Technologies"})," : HTML5, CSS3, PHP, Symfony, Angular"]}),`
+`]}),`
+`,n.jsx(e.h2,{id:"️-base-de-données",children:"🗄️ Base de Données"}),`
+`,n.jsxs(e.ul,{children:[`
+`,n.jsxs(e.li,{children:[n.jsx(e.strong,{children:"SGBD"})," : PL/SQL"]}),`
+`]}),`
+`,n.jsx(e.h2,{id:"️-système",children:"🛠️ Système"}),`
+`,n.jsxs(e.ul,{children:[`
+`,n.jsxs(e.li,{children:[n.jsx(e.strong,{children:"OS & Shell"})," : Bash, Linux"]}),`
+`]}),`
+`,n.jsx(e.h2,{id:"-logiciels",children:"🔧 Logiciels"}),`
+`,n.jsxs(e.ul,{children:[`
+`,n.jsxs(e.li,{children:[n.jsx(e.strong,{children:"Environnements & Outils"})," : Eclipse, VS Code, MobaXterm, GitHub, UML, Docker"]}),`
+`]})]})}function t(s={}){const{wrapper:e}={...l(),...s.components};return e?n.jsx(e,{...s,children:n.jsx(i,{...s})}):i(s)}export{t as default};
